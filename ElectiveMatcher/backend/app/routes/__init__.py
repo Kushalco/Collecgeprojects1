88d702ef315ec568a1ace09759b2933a921a6436
kushal-a11y <kushalstcet666@gmail.com>
2025-04-06 19:06:02 +0000

    make

--- ElectiveMatcher/backend/app/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.routes.admin_routes import admin_bp
-from app.routes.student_routes import student_bp
-from app.routes.auth_routes import auth_bp
-
-def register_routes(app):
-    app.register_blueprint(admin_bp, url_prefix="/admin")
-    app.register_blueprint(student_bp, url_prefix="/student")
-    app.register_blueprint(auth_bp, url_prefix="/auth")
